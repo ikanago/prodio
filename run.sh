708bb49e1d322dcecd29b6abb86054f42020d61f
ikanago <28985004+ikanago@users.noreply.github.com>
2020-02-11 17:30:35 +0900

    🛠 Compile and run C source within `run.sh`

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Script to compile and run assembly code.
-asm_file=$1
-obj_file=${asm_file%.*}
+source_file=$1
+asm_file=$(echo ${source_file} | sed -e "s/\.c$/\.s/")
+obj_file=${source_file%.*}
+
+cargo run -- ${source_file} -o ${asm_file}
 gcc -g -o ${obj_file} ${asm_file}
 ./${obj_file}
 echo $?
+rm ${asm_file}
 rm ${obj_file}
